b525d5339e15827fe543104efb62741db45bc514
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2026-05-04 01:44:03 +0700

    fix: add manual weight case for dense layer

--- src/shared/dense.py
+++ src/shared/dense.py
@@ -1,7 +1,15 @@
+import numpy as np
+
 class DenseLayer:
-    def __init__(self, keras_layer=None):
+    def __init__(self, keras_layer=None, input_size=None, output_size=None):
         if keras_layer is not None:
             self.weights, self.bias = keras_layer.get_weights()
+        else:
+            if input_size is not None and output_size is not None:
+                self.weights, self.bias = np.random.randn(input_size, output_size), np.random.randn(output_size)
+            else:
+                raise ValueError("NO WEIGHTS TO INITIALIZE")
+        
 
     def forward(self, x):
         self.x = x
