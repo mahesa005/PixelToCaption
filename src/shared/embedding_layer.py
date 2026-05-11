b07e7268113457a5ab7d1430b92fa63731e0260c
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2026-05-11 14:56:57 +0700

    refactor(EmbeddingLayer): change embed_dims to embed_dim

--- src/shared/embedding_layer.py
+++ src/shared/embedding_layer.py
@@ -5,20 +5,20 @@ class EmbeddingLayer:
     """
     A layer that has the function of embedding tokens into vectors.
     """
-    def __init__(self, keras_layer=None, vocab_size=None, embed_dims=None):
+    def __init__(self, keras_layer=None, vocab_size=None, embed_dim=None):
         """
         vocab_size: the total number of the words the model can learn
-        embed_dims: the dimension of the vector embedding
+        embed_dim: the dimension of the vector embedding
         """
         if keras_layer is not None:
             self.weights = keras_layer.get_weights()[0]
         else:
-            if vocab_size is not None and embed_dims is not None:
-                self.weights = np.random.randn(vocab_size, embed_dims)
+            if vocab_size is not None and embed_dim is not None:
+                self.weights = np.random.randn(vocab_size, embed_dim)
             else:
                 raise ValueError("NO WEIGHTS TO INITIALIZE")
         
-        self.embed_dims = embed_dims if embed_dims is not None else self.weights.shape[1]
+        self.embed_dim = embed_dim if embed_dim is not None else self.weights.shape[1]
 
     def forward(self, token: int):
         self.token = token
